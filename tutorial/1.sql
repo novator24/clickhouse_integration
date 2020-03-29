914b4ac4bf0d84657105b7638ed08f3fba9bb518
guestyo <basovyo@mos.ru>
2020-03-29 20:02:38 +0300

    CLI-001 up

---
+++ tutorial/1.sql
@@ -0,0 +1,30 @@
+CREATE TABLE tutorial.hits_v1
+(
+  `WatchID` UInt64,
+  `JavaEnalbe` UInt8,
+  `Title` String,
+  `GoodEvent` Int16,
+  `EventTime` DateTime,
+  `EventDate` Date,
+  `CounterID` UInt32,
+  `ClientIP` UInt32,
+  `ClientIP6` FixedString(16),
+  `RegionID` UInt32,
+  `UserID` UInt64,
+  `CounterClass` Int8,
+  `OS` UInt8,
+  `UserAgent` UInt8,
+  `URL` String,
+  `Referer` String,
+  `URLDomain` String,
+  `RefererDomain` String,
+  `Refresh` UInt8,
+  `IsRobot` UInt8,
+  `RefererCategories` Array(UInt16),
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMM(EventDate)
+ORDER BY (CounterID, EventDate, intHash32(UserID))
+SAMPLE BY intHash32(UserID)
+SETTINGS index_granularity = 8192
+
