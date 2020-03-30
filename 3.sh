a7d48c2bca9d23fd1d45d6c5dbff4e2885abdebe
guestyo <basovyo@mos.ru>
2020-03-30 18:45:46 +0300

    CLI-001 up

--- 3.sh
+++ 3.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
-set -e -x
+#!/usr/bin/expect
+# set -e -x
 # https://yadi.sk/d/ARJShvDUgazjMQ
-sudo mkdir -p /mnt/uchi_ru_clouds
-sudo spawn mount.davfs https://webdav.yandex.ru /mnt/uchi_ru_clouds/
+# sudo apt-get install -y davfs2
+# sudo apt-get install -y expect
+# sudo mkdir -p /mnt/uchi_ru_clouds
+spawn mount.davfs https://webdav.yandex.ru /mnt/uchi_ru_clouds/
 expect "Username:"
 send "USERNAME@yandex.ru\r"
 expect "Password:"
