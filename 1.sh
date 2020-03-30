115dd3c7a47f5b23d918fcc890cb84312ce7e203
guestyo <basovyo@mos.ru>
2020-03-30 18:49:21 +0300

    CLI-001 up

--- 1.sh
+++ 1.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e -x
 
-echo step 1
-
-docker run -d --name some-clickhouse-server --ulimit nofile=262144:262144 yandex/clickhouse-server
+sudo mkdir -p /mnt/uchi_ru_clouds
 
 mkdir -p $HOME/some_clickhouse_database
 docker run -d --name some-clickhouse-server -ulimit nofile=262144:262144 --volume=$HOME/some_clickhouse_database:/var/lib/c
