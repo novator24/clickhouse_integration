a4b8244b496a0ff299239f20095e2e35b9591eee
guestyo <basovyo@mos.ru>
2020-04-01 19:18:15 +0300

    CLI-003 init commit

--- 1.sh
+++ 1.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 set -e -x
-
-sudo mkdir -p /mnt/uchi_ru_clouds
-
-mkdir -p $HOME/some_clickhouse_database
-docker run -d --name some-clickhouse-server -ulimit nofile=262144:262144 --volume=$HOME/some_clickhouse_database:/var/lib/c
+docker run -d --name some-clickhouse-server -ulimit nofile=262144:262144 
