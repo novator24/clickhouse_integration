074bc2cdd43587211583a4187e71b8c49518e846
guestyo <basovyo@mos.ru>
2020-04-06 18:45:32 +0300

    CLI-002 init commit

--- 2.sh
+++ 2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e -x
-(mkdir -p uchi_ru && cd uchi_ru && cp ../Dockerfile . && docker build --rm --build-arg AIRFLOW_DEPS="datadog,dask" --build-arg PYTHON_DEPS="flask_oauthlib>=0.9" -t puckel/docker-airflow .)
+(mkdir -p uchi_ru && cd uchi_ru && cp ../Dockerfile . && mkdir -p script/ && cp ../script/entrypoint.sh ./script/ && docker build --rm --build-arg AIRFLOW_DEPS="datadog,dask" --build-arg PYTHON_DEPS="flask_oauthlib>=0.9" -t puckel/docker-airflow .)
