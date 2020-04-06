61cd9eb1cd854a4640d99642f2c94738cfb0d0b2
guestyo <basovyo@mos.ru>
2020-04-06 18:50:53 +0300

    CLI-002 init commit

--- 5.sh
+++ 5.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e -x
-(mkdir -p uchi_ru && cd uchi_ru && docker-compose -f docker-compose-CeleryExecutor.yml up -d)
+(mkdir -p uchi_ru && cd uchi_ru && cp ../docker-compose-CeleryExecutor.yml . && docker-compose -f docker-compose-CeleryExecutor.yml up -d)
